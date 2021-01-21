b7bb7d077c00f7a1fe4a98ca2b81b494a92c4fc3
Burak Kosova <burak.kosova@bahcesehir.edu.tr>
2021-01-21 08:10:20 +0300

    Added customer manager class

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        private List<Customer> customers = new List<Customer>();
+
+        public void addCustomer(Customer customer)
+        {
+            customers.Add(customer);
+            Console.WriteLine(customer.Id + " id ye sahip müşteri kaydedildi.");
+        }
+
+        public void listCustomers()
+        {
+            bool isEmpty = !customers.Any();
+            if (isEmpty)
+            {
+                Console.WriteLine("Kayıtlı müşteri bulunmamaktadır.");
+            }
+            else
+            {
+
+                foreach (Customer customer in customers)
+                {
+                    Console.WriteLine(customer.Id + " " + customer.FirstName + " " + customer.LastName + " " + customer.TC + " " + customer.AccountNumber);
+                }
+            }
+        }
+
+        public void deleteCustomer(int customerId)
+        {
+            bool found = false;
+            foreach (Customer customer in customers)
+            {
+                if (customer.Id == customerId)
+                {
+                    customers.Remove(customer);
+                    Console.WriteLine(customerId + " id li müşteri silindi.");
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                Console.WriteLine(customerId + " id ye sahip bir müşteri bulunmamaktadır.");
+            }
+        }
+    }
+}
