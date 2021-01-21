d1ddf91ac16305b2eee77544e119742251ba13a0
Burak Kosova <burak.kosova@bahcesehir.edu.tr>
2021-01-21 10:07:19 +0300

    Updated test program

--- ClassMethodDemo/Program.cs
+++ ClassMethodDemo/Program.cs
@@ -6,55 +6,51 @@ namespace ClassMethodDemo
     {
         static void Main(string[] args)
         {
-            Customer customer1 = new Customer
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.ListCustomers();
+            Console.WriteLine();
+
+            customerManager.AddCustomer(new Customer
             {
                 Id = 1,
                 FirstName = "Burak",
                 LastName = "Kosova",
                 AccountNumber = "456879731",
                 TC = "19245698764"
-            };
-            
-            Customer customer2 = new Customer
+            });
+
+            customerManager.AddCustomer(new Customer
             {
                 Id = 2,
                 FirstName = "Kendall",
                 LastName = "Jenner",
                 AccountNumber = "456687978",
                 TC = "Not TC citizen"
-            };
-            
-            Customer customer3 = new Customer
+            });
+
+            customerManager.AddCustomer(new Customer
             {
                 Id = 3,
                 FirstName = "Cem",
                 LastName = "Yılmaz",
                 AccountNumber = "442719332",
                 TC = "65478932125"
-            };
-            
-            Customer customer4 = new Customer
+            });
+
+            customerManager.AddCustomer(new Customer
             {
                 Id = 4,
                 FirstName = "Abel",
                 LastName = "Tesfaye",
                 AccountNumber = "78214658",
                 TC = "Not TC citizen"
-            };
-
-            CustomerManager customerManager = new CustomerManager();
+            });
+            Console.WriteLine();
 
             customerManager.ListCustomers();
-
-            customerManager.AddCustomer(customer1);
-            customerManager.AddCustomer(customer2);
-            customerManager.AddCustomer(customer3);
-            customerManager.AddCustomer(customer4);
             Console.WriteLine();
-            
-            customerManager.ListCustomers();
 
-            Console.WriteLine();
             customerManager.DeleteCustomer(38);
             Console.WriteLine();
 
